a3d6b6998a8965645f1692d38661f395a9448607
rohit.shaw <rohit.shaw@algonomy.com>
2023-12-04 03:42:38 +0530

    up

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzzf8rvuvSmtZBXmiHpHdyjIgIMXRg9LYTtx_8OA3BhsqfjRye2cGhc28F7B6QGz83h/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbx3QavfeeD66GEaf762XwOe71Tc5RpdMG5CX0m-ir1a_HQAqBIqHtLJA4X4wbGh-tokuA/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
